81e5394a6956f335fa7c21df5fe74e4070591db0
Anthony Beaumont <dev.xan105@outlook.be>
2021-04-07 01:07:06 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,135 @@
-# node-ini
-Yet another (opinionated) ini encoder/decoder for Node.js
+Yet another (_opinionated_) ini encoder/decoder for Node.js.
+
+Usage example
+-------------
+
+Consider an ini-file that looks like this:
+
+```ini
+; this comment is being ignored
+# that one as well
+
+scope = global
+ answer= 42
+password type = string
+yes = true
+
+[Database]
+password = some very*difficult=password:
+database-name =my-project-db
+
+[DB.default]
+datadir = /var/lib/data
+datadirWin = "C:\Windows"
+```
+
+Node
+
+```js
+//ESM
+import * as ini from '@xan105/ini';
+import { promises as fs} from 'fs';
+//CommonJS
+const ini = require('@xan105/ini');
+const { promises : fs } = require('fs');
+
+fs.readFile("path/to/ini","utf8")
+.then((content)=>{
+  const data = ini.parse(content);
+  console.log(data);
+}).catch(console.error);
+
+```
+
+Output:
+
+```json
+{
+  "scope": "global",
+  "answer": "42",
+  "password type": "string",
+  "yes": true,
+  "Database": {
+    "password": "some very*difficult=password:",
+    "database-name": "my-project-db"
+  },
+  "DB.default": {
+    "datadir": "/var/lib/data",
+    "datadirWin": "C:\\Windows"
+  }
+}
+```
+
+Install
+-------
+
+```
+npm install @xan105/ini
+```
+
+API
+---
+
+## parse(string: string, [option: obj]): obj
+
+Decode the ini-style formatted string into an object.
+
+### option ⚙️
+
+|name|type|default|description|
+|----|----|-------|-----------|
+|autoType|bool or {bool...}|{...}|Auto string to boolean / number and unquote string**|
+|ignoreGlobalSection|bool|false|Ignore keys without a section aka 'Global' section|
+|sectionFilter|string[]|[]|List of section name to filter out|
+
+### Auto type**
+
+autoType option accepts the following obj for granular control or a boolean true/false which force all options to true/false:
+
+|name|type|default|description|
+|----|----|-------|-----------|
+|bool|bool|true|String to boolean|
+|number|bool|false|String to number|
+|unquote|bool|false|Remove leading and trailing quote (" or ') in a string value|
+
+_Example_: 
+
+```js
+ini.parse(string); //default
+ini.parse(string, {autoType : true}); //every autotype to true
+ini.parse(string, {autoType : { //granular autotype
+  bool: true,
+  number: true
+}, ignoreGlobalSection: true}); //with an additional parse option
+
+```
+
+### Implementation notice
+
+- Sections cannot be nested
+- Comments are ignored (! and #)
+- Inline comments are not allowed !
+- Duplicate names : override first occurrence
+- Case sensitive
+- Name/value delimiter is "=" and is mandatory
+- Whitespace around section name, key name and key value are trimmed.
+
+## stringify(obj: obj, [option: obj]: string
+
+Encode the object obj into an ini-style formatted string.
+
+### option ⚙️
+
+|name|type|default|description|
+|----|----|-------|-----------|
+|whitespace|bool|false|Whether to put whitespace around the delimiter =|
+|blankLine|bool|true|Add blank lines between sections|
+|ignoreGlobalSection|bool|false|Ignore root properties (not under any namespace if you will)|
+|quoteString|bool|false|Quote string values using double quotes ("...")|
+
+### Implementation notice
+
+- Sections shall not be nested
+- Case sensitive
+- Empty section are allowed
+- Key value can only be a boolean, number or string
---
+++ test/test.js
@@ -0,0 +1,171 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import t from 'tap';
+import * as ini from '../lib/esm.mjs';
+
+const expected = [
+    {
+      global: "string",
+      "hel lo": "w o r l d ;inline comment are not allowed",
+      url: "http://1270.0.0.1#div",
+      '"[disturbing]"': "hey you never know",
+      update_lang: {},
+      a: {
+        override : false
+      },
+      "b.c": {
+        b : "string",
+        c : "string",
+        d : "string",
+        e : "string",
+        eq : "eq=eq",
+        f : "\'string\'",
+        g : '\"string\"'
+      },
+      d: {
+        d: "125",
+        e: "0x125",
+        f: '\"125\"',
+        g: "0123456",
+        h: "8M"
+      },
+      e: {
+        path: '"var/lib/path/to"',
+        win32: '"C:\\\\Windows\\\\System32"'
+      },
+      f: {
+        "void" : "",
+        empty: '""',
+        mixed: `"something'`
+      },
+      h: {
+        lessIsMore : '\"\\.\"',
+        lessIsMore2 : "\\."
+      },
+      i: {},
+      j: {
+        yes: true,
+        yes2: '\"true\"',
+        no: false,
+        no2: '\"false\"'
+      },
+      Database: {
+        password: "some very*difficult=password:",
+        "database-name": "my-project-db"
+      }
+    },
+    {
+      global: "string",
+      "hel lo": "w o r l d ;inline comment are not allowed",
+      url: "http://1270.0.0.1#div",
+      '"[disturbing]"': "hey you never know",
+      update_lang: {},
+      a: {
+        override : false
+      },
+      "b.c": {
+        b : "string",
+        c : "string",
+        d : "string",
+        e : "string",
+        eq : "eq=eq",
+        f : "string",
+        g : "string"
+      },
+      d: {
+        d: 125,
+        e: "0x125",
+        f: 125,
+        g: 123456,
+        h: "8M"
+      },
+      e: {
+        path: "var/lib/path/to",
+        win32: "C:\\\\Windows\\\\System32"
+      },
+      f: {
+        "void" : "",
+        empty: "",
+        mixed: `"something'`
+      },
+      h: {
+        lessIsMore : "\\.",
+        lessIsMore2 : "\\."
+      },
+      i: {},
+      j: {
+        yes: true,
+        yes2: true,
+        no: false,
+        no2: false
+      },
+      Database: {
+        password: "some very*difficult=password:",
+        "database-name": "my-project-db"
+      }
+    },
+    {
+      update_lang: {},
+      a: {
+        override : "false"
+      },
+      "b.c": {
+        b : "string",
+        c : "string",
+        d : "string",
+        e : "string",
+        eq : "eq=eq",
+        f : "\'string\'",
+        g : '\"string\"'
+      },
+      d: {
+        d: "125",
+        e: "0x125",
+        f: '\"125\"',
+        g: "0123456",
+        h: "8M"
+      },
+      e: {
+        path: '"var/lib/path/to"',
+        win32: '"C:\\\\Windows\\\\System32"'
+      },
+      f: {
+        "void" : "",
+        empty: '""',
+        mixed: `"something'`
+      },
+      h: {
+        lessIsMore : '\"\\.\"',
+        lessIsMore2 : "\\."
+      },
+      i: {},
+      j: {
+        yes: "true",
+        yes2: '\"true\"',
+        no: "false",
+        no2: '\"false\"'
+      }
+    },
+  ];
+
+t.test('basic', t => {
+
+  const sample = path.resolve("./test/sample/basic.ini");
+  const content = fs.readFileSync(sample,"utf8");
+
+  t.same(ini.parse(content), expected[0], 'parsing default option');
+  t.same(ini.parse(content, {autoType: true}), expected[1], 'parsing autotype on');
+  t.same(ini.parse(content, {autoType: {bool: true, number: true, unquote: true}}), expected[1], 'parsing autotype all');
+  t.same(ini.parse(content, {autoType: false, sectionFilter: ["Database"], ignoreGlobalSection: true}), expected[2], 'parsing autotype off and filter and no global');
+  t.end();
+});
+
+t.test('read write read equivalence', t => {
+  const originalFile = path.resolve("./test/sample/php.ini");
+  const originalData = ini.parse(fs.readFileSync(originalFile,"utf8"));
+  const data = ini.parse(ini.stringify(originalData));
+  t.strictSame(data, originalData, 'is stringify producing same result as original');
+
+  t.same(ini.parse(ini.stringify({hello: "world"},{quoteString: true})),{hello: '\"world\"'},"quoted");
+  t.end();
+});
